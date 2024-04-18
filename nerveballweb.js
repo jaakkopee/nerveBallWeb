081a433763b6867d2030b500f5f933247798a458
jaakkopee <iipekaj@gmail.com>
2024-04-18 19:25:32 +0300

    second

--- nerveballweb.js
+++ nerveballweb.js
@@ -8,9 +8,11 @@ function setup() {
 
 function draw() {
   background(0);
-  for (var x = 0; x <= width; x += 50) {
-    for (var y = 0; y <= height; y += 50) {
-      ellipse(x, y, 25, 25);
+  fill(random(255), random(255), random(255));
+  for (var i=0; i<100; i+=1) {
+    background(0);
+    for (var j=0; j<100; j+=1) {
+      ellipse(random(width), random(height), 10, 10);
     }
   }
 }
