1314d86030c6649c45eb10386097fc997e945350
jaakkopee <iipekaj@gmail.com>
2024-04-22 04:26:12 +0300

    x

--- nerveballweb.js
+++ nerveballweb.js
@@ -386,7 +386,7 @@ function displayTime() {
 
 function displayPoints() {
     totalscore = player_points + player_lastSplitPoints;
-    document.getElementById("points").innerHTML = Math.round(totalscore) + "(" + Math.round(player_lastSplitPoints) + ") Balls:" + ball_amount;
+    document.getElementById("points").innerHTML = Math.round(totalscore) + " (" + Math.round(player_lastSplitPoints) + ")";
 }
 
 function addToTime(time) {
